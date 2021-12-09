f8b07693c70ae83fc8871dbe16c230dd4891dd73
ipCoelho <israelcontatoprofissional@gmail.com>
2021-12-09 10:23:21 -0300

    projeto atulizado

--- route/usuario/index.php
+++ route/usuario/index.php
@@ -4,9 +4,9 @@
     header("Access-Control-Allow-Methods: *");
     header("Content-Type: application/json");
 
-    include("./controller/usuarioController.php");
-    include("./model/Usuario.php");
-    include("./Connection.php");
+    include("../controller/usuarioController.php");
+    include("../model/Usuario.php");
+    include("../Connection.php");
 
     $conexao = new Connection();
     $modelUsuario = new Usuario($conexao->conectar());
